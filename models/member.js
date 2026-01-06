37482c7d2d3554719bb19e26938f726cd90afa82
Hanan-k20 <98twinshanan@gmail.com>
2026-01-06 13:32:57 +0300

    conflict

--- models/member.js
+++ models/member.js
@@ -1,48 +1,50 @@
 //requir mogose library
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 //creat the mongoose schema
 const duckShema = new mongoose.Schema({
-     firstName: {
+  firstName: {
     type: String,
     required: true,
   },
-    lastName:{
-        type: String,
-        required: true
-    },
+  lastName: {
+    type: String,
+    required: true
+  },
   gender: {
     type: String,
     required: true
   },
-  dateOfBirth:{
-    type:String,
-  required:true
+  dateOfBirth: {
+    type: String,
+    required: true
   },
-  image:{
-type: String,
-required:true},
-  generation: { 
-type: Number,
-min:1, // رقم الجيل اذا1 الجد اذا 2 الاب اذا 3 الاولاد 
-required: true },
-// 1.نربط الشجره بالعائله لنستخدم المكتبه 
-  treeId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Tree', 
-    required: true 
+  image: {
+    type: String,
+    required: true
+  },
+  generation: {
+    type: Number,
+    min: 1, // رقم الجيل اذا1 الجد اذا 2 الاب اذا 3 الاولاد 
+    required: true
+  },
+  // 1.نربط الشجره بالعائله لنستخدم المكتبه 
+  treeCode: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tree',
+    required: true
   },
 
   // 2. ربط الشخص بوالده 
-  parentId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Duck', 
+  parentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Duck',
     default: null //اذا اخترنا null يكون الجد 
   }
 })
 
 //initialize the mogose model
-const Duck = mongoose.model('Duck',duckShema)
+const Duck = mongoose.model('Duck', duckShema)
 
 //export the model
 module.exports = Duck;
