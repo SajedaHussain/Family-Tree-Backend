0f73152cc80373dfc8a84bef03a958223161a823
SajedaHussain <sajedataqi22@gmail.com>
2026-01-07 10:40:29 +0300

    chearify the parameters

--- controllers/members.js
+++ controllers/members.js
@@ -1,13 +1,12 @@
-// controllers/Members.js
+const express = require('express')
 //requir the model
 const Member = require('../models/member');
 const Tree = require('../models/tree');
-//require express
-const express = require('express')
-
 //initialize the router
 const router =express.Router();
 
+
+//Acess Code ====================================================================================================
 const verifyAccess = async (tree_id, code) => {
     const targetTree = await Tree.findById(tree_id);
     if (!targetTree)                    
@@ -17,8 +16,7 @@ const verifyAccess = async (tree_id, code) => {
    return { success: true };
 };
 
-
-//create a new ..post/ducks/
+//POST ===========================================================================================================
 router.post('/',async (req,res)=>{
     try{
         const { tree_id, code } = req.body; 
@@ -31,18 +29,16 @@ router.post('/',async (req,res)=>{
         res.status(201).json({ member });
     }catch(error){
         console.log(error)
-        res.status(500).json({error:"fail to create member"}) // 500 means error
+        res.status(500).json({error:"fail to create member"})
     }
 })
 
-// READ - GET -(index) /ducks - GET+/ducks
+// GET All =====================================================================================================
 router.get('/', async (req, res) => {
-  // Setting up for our code
   try{
    const { tree_id, code } = req.query;
     let filter = {};
        if (tree_id) filter.tree_id = tree_id;//اذا ارسل treeId سيقوم باحضار البط للشجره المحدده 
-
      const member = await Member.find(filter).populate('parentId', 'firstName lastName');//populate-->{ابحث عن الشخص الذي لديه ال ID و احضره لهنا }
      //البارامتر الأولparentId-->(اين نريد تعبئه البيانات )  //البارامتر الثاني ('firstName lastName')-->المعلومات التي نريد تعبئتها 
      res.status(200).json({ member });
@@ -52,11 +48,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-// show route for one record :id -GET+/ducks/123
+// GET ONE ============================================================================================================
 router.get('/:id',async(req,res)=>{
     try{
         //git the id from the param then find-by id
-        // if we dont get a duck respond with 404 else send 200 with duck
          const {id} =req.params
          const member = await Member.findById(id)
          if(!member){  // if there is no duck wich is null 
@@ -70,7 +65,7 @@ router.get('/:id',async(req,res)=>{
     }
 })
 
-//delete a duck -DEL+/ducks/123
+//DELET =========================================================================================================
 router.delete('/:id',async(req,res)=>{
     try{
         const { tree_id, code } = req.body; 
@@ -80,10 +75,10 @@ router.delete('/:id',async(req,res)=>{
         if (access.error) return res.status(access.status).json({ error: access.error });
        
         const member = await Member.findByIdAndDelete(req.params.id)  //try to find and delete the duck using the id
-        if(!member){      //if there is no duck(not null wich is true), send 404
+        if(!member){      
             res.status(404).json({error:"member not found"})
-        }else{         //else send back a msg ssys deleted
-            res.status(200).json({member}) //204 most pouler for deleting it means deleted and no data will be send- json send the duck
+        }else{        
+            res.status(200).json({member}) 
         }
         
          }
@@ -94,7 +89,7 @@ router.delete('/:id',async(req,res)=>{
 })
 
 
-//updating - PUT + /ducks/123
+//PUT ==============================================================================================================
 router.put('/:id',async(req,res)=>{
     try{
         const { tree_id, code } = req.body; 
