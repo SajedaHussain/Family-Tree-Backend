8f18439588b09d76f9df8b947261137765710959
SajedaHussain <sajedataqi22@gmail.com>
2026-01-07 10:46:59 +0300

    last formating

--- controllers/members.js
+++ controllers/members.js
@@ -6,7 +6,7 @@ const Tree = require('../models/tree');
 const router = express.Router();
 
 
-//Acess Code ====================================================================================================
+//ACCESS CODE ====================================================================================================
 const verifyAccess = async (tree_id, code) => {
     const targetTree = await Tree.findById(tree_id);
     if (!targetTree)
@@ -21,27 +21,28 @@ router.post('/', async (req, res) => {
     try {
         const { tree_id, code } = req.body;
         const access = await verifyAccess(tree_id, code);
-
         if (access.error)
             return res.status(access.status).json({ error: access.error });
 
         const member = await Member.create(req.body);
         res.status(201).json({ member });
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: "fail to create member" })
     }
 })
 
-// GET All =====================================================================================================
+// GET ALL =====================================================================================================
 router.get('/', async (req, res) => {
     try {
-        const { tree_id, code } = req.query;
+        const { tree_id } = req.query;
         let filter = {};
         if (tree_id) filter.tree_id = tree_id;//اذا ارسل treeId سيقوم باحضار البط للشجره المحدده 
         const member = await Member.find(filter).populate('parentId', 'firstName lastName');//populate-->{ابحث عن الشخص الذي لديه ال ID و احضره لهنا }
         //البارامتر الأولparentId-->(اين نريد تعبئه البيانات )  //البارامتر الثاني ('firstName lastName')-->المعلومات التي نريد تعبئتها 
         res.status(200).json({ member });
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: "fail to get member" })
@@ -59,21 +60,19 @@ router.get('/:id', async (req, res) => {
         } else {
             res.status(200).json({ member })
         }
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: 'faild to get a member' })
     }
 })
 
-//DELET =========================================================================================================
+//DELETE =========================================================================================================
 router.delete('/:id', async (req, res) => {
     try {
         const { tree_id, code } = req.body;
-
         const access = await verifyAccess(tree_id, code);
-
         if (access.error) return res.status(access.status).json({ error: access.error });
-
         const member = await Member.findByIdAndDelete(req.params.id)  //try to find and delete the duck using the id
         if (!member) {
             res.status(404).json({ error: "member not found" })
@@ -81,8 +80,7 @@ router.delete('/:id', async (req, res) => {
             res.status(200).json({ member })
         }
 
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error)
         res.status(500).json({ error: 'faild to get a member' })
     }
@@ -92,11 +90,8 @@ router.delete('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try {
         const { tree_id, code } = req.body;
-
         const access = await verifyAccess(tree_id, code);
-
         if (access.error) return res.status(access.status).json({ error: access.error });
-
         const { id } = req.params;
         const member = await Member.findByIdAndUpdate(id, req.body, { new: true })
         if (!member) {
@@ -104,6 +99,7 @@ router.put('/:id', async (req, res) => {
         } else {
             res.status(200).json({ member })
         }
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: "faild to update" })
