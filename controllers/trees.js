2d763c31acd8c522e5763a70fb89299d5dd983c1
SajedaHussain <sajedataqi22@gmail.com>
2026-01-07 10:33:52 +0300

    last formating

--- controllers/trees.js
+++ controllers/trees.js
@@ -1,84 +1,85 @@
-const express=require('express')
+const express = require('express')
 //require the moudle
 const Tree = require('../models/tree.js');
 //inilialize the router
-const router=express.Router();
-
+const router = express.Router();
 
 
 //POST ==========================================================================================================
-router.post('/',async (req,res)=>{
+router.post('/', async (req, res) => {
     try {
         const tree = await Tree.create(req.body)
-        res.status(201).json({tree})
+        res.status(201).json({ tree })
     } catch (error) {
         console.log(error)
-        res.status(500).json({error:"failed to create tree"})
+        res.status(500).json({ error: "failed to create tree" })
     }
 })
 
 //Get =============================================================================================================
-router.get('/',async(req,res)=>{
+router.get('/', async (req, res) => {
     try {
-        const tree= await Tree.find({})
-        res.status(200).json({tree})
+        const tree = await Tree.find({})
+        res.status(200).json({ tree })
     } catch (error) {
         console.log(error)
-        res.status(500).json({error:"failed to get tree"}) 
+        res.status(500).json({ error: "failed to get tree" })
     }
 
 })
+
 //Get on Tree =====================================================================================================
-router.get('/:id', async(req,res)=>{
+router.get('/:id', async (req, res) => {
     try {
-        const{id}=req.params
+        const { id } = req.params
         const tree = await Tree.findById(id)
         if (!tree) {
-            res.status(404).json({error:"tree not found"})
+            res.status(404).json({ error: "tree not found" })
         } else {
-            res.status(200).json({tree})
+            res.status(200).json({ tree })
 
         }
-        
+
     } catch (error) {
         console.log(error)
-        res.status(500).json({error:"failed to get tree"}) 
-     
+        res.status(500).json({ error: "failed to get tree" })
+
     }
 })
+
 //Delete ============================================================================================================== 
-router.delete('/:id',async(req,res)=>{
+router.delete('/:id', async (req, res) => {
     try {
-      const{id}=req.params
-      const tree = await Tree.findByIdAndDelete(id)
-       if (!tree) {
-            res.status(404).json({error:"Tree not found"})
+        const { id } = req.params
+        const tree = await Tree.findByIdAndDelete(id)
+        if (!tree) {
+            res.status(404).json({ error: "Tree not found" })
         } else {
             //use status 204 if dont 
             res.status(200).json({ message: "Tree deleted" })
         }
-        
+
     } catch (error) {
-      console.log(error)
-      res.status(500).json({error:"failed to delete tree"})    
+        console.log(error)
+        res.status(500).json({ error: "failed to delete tree" })
     }
 })
+
 //PUT =========================================================================================================
-router.put('/:id', async(req,res)=>{
+router.put('/:id', async (req, res) => {
     try {
-       const{id}=req.params
-      const tree = await Tree.findByIdAndUpdate(id,req.body,{new: true})
-       if (!tree) {
-            res.status(404).json({error:"tree not found"})
-        } else { 
-            res.status(200).json({tree})
+        const { id } = req.params
+        const tree = await Tree.findByIdAndUpdate(id, req.body, { new: true })
+        if (!tree) {
+            res.status(404).json({ error: "tree not found" })
+        } else {
+            res.status(200).json({ tree })
         }
     } catch (error) {
-      console.log(error)
-      res.status(500).json({error:"failed to update tree"})   
+        console.log(error)
+        res.status(500).json({ error: "failed to update tree" })
     }
 })
 
 //export the router===============================================================================================
-
-module.exports=router;
+module.exports = router;
