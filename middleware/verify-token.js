8da70a16ccb31ba4f23b2b0e34618721291524c8
Hanan-k20 <98twinshanan@gmail.com>
2026-01-08 11:39:18 +0300

    last

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
   try {
+    
     const token = req.headers.authorization.split(' ')[1];
     console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
